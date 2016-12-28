a250910d113b8abe21f9f05ea9208403af1d6211
refluster <refluster@gmail.com>
2016-12-28 17:57:50 +0900

    refs #98 read and output raw gyro data

--- driver/sensor/main.cc
+++ driver/sensor/main.cc
@@ -5,17 +5,30 @@
 #include <wiringPi.h>
 #include <wiringPiI2C.h>
 
+int twos_compliment(int val) {
+    if (val >= 0x8000) {
+        return -((65535 - val) + 1);
+	} else {
+        return val;
+	}
+}	
+
 int main()
 {
 	int fd,ret;
+	int raw_gyro_data[3];
+	int raw_gyro_accel_data[3];
 
 	fd = wiringPiI2CSetup(0x68);
-	printf("setup return : %d\n",fd);
+	printf("setup return : %d\n", fd);
 
-	ret = wiringPiI2CReadReg8(fd, 0x75);
-	printf("0x75 : %d\n",ret);
+	raw_gyro_data[0] = wiringPiI2CReadReg16(fd, 0x43);
+	raw_gyro_data[1] = wiringPiI2CReadReg16(fd, 0x45);
+	raw_gyro_data[2] = wiringPiI2CReadReg16(fd, 0x47);
 
-	ret = wiringPiI2CReadReg8(fd, 0x6b);
-	printf("0x6b : %d\n",ret);
+	printf("gyro_x : %d\n", raw_gyro_data[0]);
+	printf("gyro_y : %d\n", raw_gyro_data[1]);
+	printf("gyro_z : %d\n", raw_gyro_data[2]);
+
 	return 0;
 }
