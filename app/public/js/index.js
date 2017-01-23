0de33c4a6a80428e586e790cdca5a4ff2ab2ac13
refluster <refluster@gmail.com>
2017-01-23 19:54:38 +0900

    #99 display thumbnail

---
+++ app/public/js/index.js
@@ -0,0 +1,29 @@
+app = angular.module('App', ['ngRoute']);
+
+app.config(['$routeProvider', function ($routeProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl: 'index-tmpl',
+			controller: 'cList'
+		})
+        .otherwise({
+            redirectTo: '/'
+        });
+}]);
+
+app.controller('cMain', ['$scope', function($scope) {
+}]);
+
+app.controller('cList', ['$scope', function($scope) {
+	$scope.ctx = document.getElementById('virtual-canvas').getContext('2d');
+	$scope.thumb = document.getElementById('thumb');
+
+	document.getElementById('file').addEventListener('change', function() {
+		var fileReader = new FileReader() ;
+		fileReader.onload = function() {
+			$scope.thumb.setAttribute('src', this.result);
+		}
+		fileReader.readAsDataURL(this.files[0]);
+	});
+}]);
+
